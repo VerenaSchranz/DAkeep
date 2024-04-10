7982c2a20a6c0f4f481a960411383f462f4505e5
VerenaSchranz <152481843+VerenaSchranz@users.noreply.github.com>
2024-04-10 11:04:43 +0200

    Update note-list.service.ts

--- src/app/firebase-services/note-list.service.ts
+++ src/app/firebase-services/note-list.service.ts
@@ -1,34 +1,59 @@
-import { Injectable, inject } from '@angular/core';
-import { Note } from '../interfaces/note.interface';
-import { Firestore, collection,  doc, collectionData } from '@angular/fire/firestore';
-import { Observable } from 'rxjs';
+import { Injectable, inject } from "@angular/core";
+import { Note } from "../interfaces/note.interface";
+import {
+  Firestore,
+  collection,
+  doc,
+  collectionData,
+  onSnapshot,
+} from "@angular/fire/firestore";
+import { Observable } from "rxjs";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class NoteListService {
-items$;
-items;
+  items$;
+  items;
+
+  unsubList;
+  unsubSingle;
+
   firestore: Firestore = inject(Firestore);
-  constructor() { 
-    this.items$ = collectionData(this.getNotesRef());
-    this.items = this.items$.subscribe( (list) => {
-      list.forEach(element => {
-        
+  constructor() {
+    this.unsubList = onSnapshot(this.getNotesRef(), (list) => {
+      list.forEach((element) => {
+        console.log(element);
       });
-    })
+
+      this.unsubList = onSnapshot(
+        this.getSingleDocRef("notes", "cMIdj9VB5E78C56UxB9n"), (element) => {
+        });
+    });
+
+    this.unsubSingle();
+    this.unsubList();
+
+
+
+
+    this.items$ = collectionData(this.getNotesRef());
+    this.items = this.items$.subscribe((list) => {
+      list.forEach((element) => {
+        console.log(element);
+      });
+    });
     this.items.unsubscribe();
   }
-  // const itemCollection = collection(this.firestore, 'items');
 
-  getNotesRef( ){
-    return collection(this.firestore, 'notes');
+  getNotesRef() {
+    return collection(this.firestore, "notes");
   }
-  getTrashRef( ){
-    return collection(this.firestore, 'trash');
+  getTrashRef() {
+    return collection(this.firestore, "trash");
   }
 
-  getSingleDocRef(colId:string, docId:string) {
-    return doc(collection(this.firestore, colId), docId)
+  getSingleDocRef(colId: string, docId: string) {
+    return doc(collection(this.firestore, colId), docId);
   }
 }
