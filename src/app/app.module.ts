cee2ac170da0532e920fbf519bbdfd2b09734fe3
VerenaSchranz <152481843+VerenaSchranz@users.noreply.github.com>
2024-04-10 10:22:59 +0200

    firestor adding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,8 @@ import { NoteListComponent } from './note-list/note-list.component';
 import { NoteComponent } from './note-list/note/note.component';
 import { FormsModule } from '@angular/forms';
 import { AddNoteDialogComponent } from './add-note-dialog/add-note-dialog.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 
 @NgModule({
@@ -23,6 +25,8 @@ import { AddNoteDialogComponent } from './add-note-dialog/add-note-dialog.compon
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+    provideFirebaseApp(() => initializeApp({"projectId":"danotes-f3e72","appId":"1:704925282940:web:eb0066043decec10cd8ff9","storageBucket":"danotes-f3e72.appspot.com","apiKey":"AIzaSyC8rQ7N7scEhACJOvJxuzxLRpS5F2AYxA4","authDomain":"danotes-f3e72.firebaseapp.com","messagingSenderId":"704925282940"})),
+    provideFirestore(() => getFirestore()),
   ],
   providers: [],
   bootstrap: [AppComponent]
